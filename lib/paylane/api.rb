f0841affb45fdaf2867337fa882566cf0a860cef
Bartlomiej Kozal <bkzl@me.com>
2012-07-17 13:04:34 +0200

    Extract making request to separate method

--- lib/paylane/api.rb
+++ lib/paylane/api.rb
@@ -5,31 +5,30 @@ module PayLane
     end
 
     def multi_sale(params)
-      response = @client.request(:multiSale) do
-        soap.body = {'multi_sale_params' => params}
-      end
-      response.to_hash[:multi_sale_response][:response]
+      do_request(:multiSale, params, 'multi_sale_params')
+        .to_hash[:multi_sale_response][:response]
     end
 
     def get_sale_result(params)
-      response = @client.request(:getSaleResult) do
-        soap.body = params
-      end
-      response.to_hash[:get_sale_result_response][:response]
+      do_request(:getSaleResult, params)
+        .to_hash[:get_sale_result_response][:response]
     end
 
     def check_sales(params)
-      response = @client.request(:checkSales) do
-        soap.body = {'check_sales_params' => params}
-      end
-      response.to_hash[:check_sales_response][:check_sales_response]
+      do_request(:checkSales, params, 'check_sales_params')
+        .to_hash[:check_sales_response][:check_sales_response]
     end
 
     def resale(params)
-      response = @client.request(:resale) do
-        soap.body = params
-      end
-      response.to_hash[:resale_response][:response]
+      do_request(:resale, params)
+        .to_hash[:resale_response][:response]
+    end
+
+    private
+
+    def do_request(method, params, params_prefix = nil)
+      body = params_prefix ? {params_prefix => params} : params
+      @client.request(method) { soap.body = body }
     end
   end
 end
