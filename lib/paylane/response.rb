c32acbdcf6b1e920f142422308b3a637a2790405
Bartlomiej Kozal <bkzl@me.com>
2012-08-06 09:58:41 +0200

    Add Response#attributes

--- lib/paylane/response.rb
+++ lib/paylane/response.rb
@@ -9,15 +9,24 @@ module PayLane
     end
 
     def has_ok?
-      !@response[:ok].nil?
+      @response.include?(:ok)
     end
 
     def has_error?
       !has_ok?
     end
 
+    def has_data?
+      @response.include?(:data)
+    end
+
     def error_description
        @response[:error][:error_description] if has_error?
+    end
+
+    def attributes
+      hash = (@response[:ok] || @response[:error])
+      has_data? ? hash.merge(@response[:data]) : hash
     end
   end
 end
--- spec/paylane/resposne_spec.rb
+++ spec/paylane/resposne_spec.rb
@@ -1,32 +1,49 @@
 require 'spec_helper'
 
 describe PayLane::Response do
-  let(:ok_response) { PayLane::Response.new(ok: {}) }
-  let(:error_response) { PayLane::Response.new(error: {error_description: 'Something went wrong'}) }
+  context 'has :ok and :data hashes' do
+    let(:response) { PayLane::Response.new(ok: {sale_id: 1}, data: {fraud_score: 1.0}) }
 
-  describe '#success?' do
-    it "returns true if response has :ok key" do
-      ok_response.has_ok?.should be_true
+    it "returns true on #has_ok?" do
+      response.has_ok?.should be_true
     end
 
-    it "returns false if resposne hasn't :ok key" do
-      error_response.has_ok?.should be_false
-    end
-  end
-
-  describe '#error?' do
-    it "returns true if response has :error key" do
-      error_response.has_error?.should be_true
+    it "returns false on #has_error?" do
+      response.has_error?.should be_false
     end
 
-    it "returns false if resposne hasn't :error key" do
-      ok_response.has_error?.should be_false
+    it "returns content of :ok key on #attributes" do
+      response.attributes.should == {sale_id: 1, fraud_score: 1.00}
+    end
+
+    it "returns true on #has_data?" do
+      response.has_data?.should be_true
     end
   end
 
-  describe '#error_description' do
-    it 'returns error description' do
-      error_response.error_description.should == 'Something went wrong'
+  context 'has :error hash' do
+    let(:response) do
+      PayLane::Response.new(error: {error_description: 'Something went wrong'})
+    end
+
+    it "returns false on #has_ok?" do
+      response.has_ok?.should be_false
+    end
+
+    it "returns true on #has_error?" do
+      response.has_error?.should be_true
+    end
+
+    it "returns error description" do
+      response.error_description.should == 'Something went wrong'
+    end
+
+    it "returns content of :error key on #attributes" do
+      response.attributes.should == {error_description: 'Something went wrong'}
+    end
+
+    it "returns false on #has_data?" do
+      response.has_data?.should be_false
     end
   end
 end
